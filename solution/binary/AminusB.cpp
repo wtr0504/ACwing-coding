6a16719db70e4019b4be5f057209b5ad5f9591d1
wangtaoran <wangtaoran0504nb@outlook.com>
2025-06-17 21:05:23 +0800

    coding

--- solution/binary/AminusB.cpp
+++ solution/binary/AminusB.cpp
@@ -1,35 +1,21 @@
 #include "../common.hpp"
 #include <algorithm>
 
-vector<int> v(200005,0);
 
-int helper(int target,int l,int r){
-    
-    while(l < r){
-        int mid = (l + r)/2;
-        if(v[mid] >= target){
-            r = mid;
-        } else{
-            l = mid + 1;
-        }
+
+long a[200001];
+long N,C,ans;
+void solution()
+{
+    cin>>N>>C;
+    for(int i=1;i<=N;i++)
+    {
+        cin>>a[i];
     }
-    if(v[l] == target) return 1;
-    return 0;
-}
-
-void solution(){
-    int n,c;
-    cin>>n>>c;
-    for(int i = 0;i < n;i++){
-        cin>>v[i];
+    sort(a+1,a+N+1);
+    for(int i=1;i<=N;i++)
+    {
+        ans+=((upper_bound(a+1,a+N+1,a[i]+C)-a)-(lower_bound(a+1,a+N+1,a[i]+C)-a));
     }
-    sort(v.begin(),v.begin() + n - 1);
-    int res = 1;
-    int i = 0,j = n-1;
-    for(int i = 0;i < n;i++){
-        
-
-    }
-
-
+    cout<<ans<<endl;
 }
